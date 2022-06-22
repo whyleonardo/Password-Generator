eaf7f2b49310fee49eaa23fc6a1fd906f0c9c119
whyleonardo <pleasefloyd73@gmail.com>
2022-06-22 12:47:31 -0300

    feat: js finalized

--- main.js
+++ main.js
@@ -7,31 +7,53 @@ const inputPassword = document.getElementById('password-value')
 const buttonCopy = document.getElementById('copy-clipboard')
 const buttonReset = document.getElementById('reset')
 
+let newPassword = ''
+let passwordIsGenerated = false
+
+// Atualização constante no valor do slider
 const showSliderValue = setInterval(() => {
   sliderDisplayNumber.innerHTML = slider.value
 }, 10)
 
+// Gerar password
 const newPasswordGenerated = () => {
   const charset =
     'abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$$%'
   const charsetArray = charset.split('')
   newPassword = charsetArray.sort(() => Math.random() - 0.5)
   newPassword.length = slider.value
+  newPassword = newPassword.join('')
 
-  inputPassword.setAttribute('value', newPassword.join(''))
+  passwordIsGenerated = true
+  if (passwordIsGenerated === true) {
+    passwordContainer.classList.remove('hidden')
+  }
+
+  inputPassword.setAttribute('value', newPassword)
+  return newPassword
 }
 
+// Copiar o password
+function copyPassword() {
+  navigator.clipboard.writeText(newPassword)
+  alert('Copiado com sucesso')
+}
+
+// Mostrar o password
 function showPassword() {
   inputPassword.removeAttribute('type')
 }
 
+// Resetar o password gerado
+function resetPassword() {
+  passwordIsGenerated = false
+  passwordContainer.classList.add('hidden')
+  inputPassword.setAttribute('value', '')
+  inputPassword.setAttribute('type', 'password')
+
+  slider.value = 5
+}
+
 // to do
 
-// copiar texto
-// resetar o inputPassword
-// pensar em novos nomes pras variáveis
 // esconder a div do menu enquanto o password não foi gerado ainda
-
-// function copyPassword() {
-//   navigator.clipboard.writeText(newPassword)
-// }
